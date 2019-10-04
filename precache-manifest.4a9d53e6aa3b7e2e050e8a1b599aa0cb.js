7971661f8fb9b1e6a49043582f3bcb57b0c8f481
Boyuan Fang <b3fang@ucsd.edu>
2019-10-03 23:58:23 -0700

    Updates

---
+++ precache-manifest.4a9d53e6aa3b7e2e050e8a1b599aa0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c618828704edb94261cb3db6ed8e56c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cef1863cc7c62aa7a96",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1ce69f833a26d47c706e",
+    "url": "/static/js/2.b86b068e.chunk.js"
+  },
+  {
+    "revision": "2cef1863cc7c62aa7a96",
+    "url": "/static/js/main.17387a22.chunk.js"
+  },
+  {
+    "revision": "264f2b72b060f3f06908",
+    "url": "/static/js/runtime-main.4d3d5152.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
